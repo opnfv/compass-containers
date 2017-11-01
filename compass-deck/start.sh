c992d3ac76de608bc4b4e36e8fd53c376e1602ac
Harry Huang <huangxiangyu5@huawei.com>
2017-11-01 11:56:50 +0800

    Add compasss-deck
    RESTful API and DB Handlers for Compass
    
    Change-Id: I1ce411f279943764c286ea48dca9185d453cf254
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

---
+++ compass-deck/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+until mysql -h"compass-db" -u"root" -p"root" -e 'show databases'; do
+  >&2 echo "DB is unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "DB is up"
+systemctl start httpd
