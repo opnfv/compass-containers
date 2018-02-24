6db2a303546afa13d356f93eb26e76034a7208ef
Harry Huang <huangxiangyu5@huawei.com>
2018-02-24 15:38:47 +0800

    Seprate compass-tasks
    JIRA: -
    
    Keep three compass-tasks:
      compass-tasks - merge both OSA and kubespray
      compass-tasks-osa - only OSA
      compass-tasks-k8s - only kubespray
    
    Change-Id: Ia790da1b5080322c5e2efe0980aabe9a76867c10
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

---
+++ compass-tasks-k8s/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum install git ntp wget ntpdate openssh-server python-devel sudo '@Development Tools' -y
+#yum install -y python-yaml
+systemctl stop firewalld
+systemctl mask firewalld
+
+# get kubespray code and install requirement
+rm -rf  /opt/kargo_k8s
+git clone https://github.com/kubernetes-incubator/kubespray.git /opt/kargo_k8s
+cd /opt/kargo_k8s
+git checkout f4180503c891bea4b4b77a2f7cc93923411a7449 -b k8s1.9.1
+pip install ansible==2.4.2.0
