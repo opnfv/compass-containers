750e957507f9c9f5cd4d843898a5f9087ee2c00c
Harry Huang <huangxiangyu5@huawei.com>
2019-02-14 09:46:46 +0800

    Modify compass-tasks-k8s
    JIRA: -
    
    Upgrade ansible inside virtualenv and link ansible
    binaries outside.
    
    Change-Id: Ic41bea94dd25cd1ea99f3a27a100be6e7f9277dd
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

--- compass-tasks-k8s/run.sh
+++ compass-tasks-k8s/run.sh
@@ -10,4 +10,7 @@ rm -rf  /opt/kargo_k8s
 git clone https://github.com/kubernetes-incubator/kubespray.git /opt/kargo_k8s
 cd /opt/kargo_k8s
 git checkout f4180503c891bea4b4b77a2f7cc93923411a7449 -b k8s1.9.1
+source /root/.virtualenvs/compass-core/bin/activate
 pip install ansible==2.4.2.0
+ln -s /root/.virtualenvs/compass-core/bin/ansible /usr/bin/ansible
+ln -s /root/.virtualenvs/compass-core/bin/ansible-playbook /usr/bin/ansible-playbook
